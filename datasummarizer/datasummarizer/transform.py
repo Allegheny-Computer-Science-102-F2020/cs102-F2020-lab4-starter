aa1d2ebffef362d560cdbf6ffec5637c0447af01
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-05 14:25:37 -0400

    Add transform_string_to_number_list with TODO markers in transform.

---
+++ datasummarizer/datasummarizer/transform.py
@@ -0,0 +1,12 @@
+"""Transform the data."""
+
+from typing import List
+
+
+def transform_string_to_number_list(data_text: str) -> List[float]:
+    """Transform a string of numerical values to a list of numbers."""
+    # TODO: Create an empty list to store the numerical values
+    # TODO: For each individual number in the text file,
+    # convert it to a floating point value and append it
+    # to the list of numerical values
+    # TODO: return the list of numerical values
