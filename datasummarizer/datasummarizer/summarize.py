01bdfd602960fd231ae6d83ed969f54e7b2b60b9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-05 14:22:56 -0400

    Add the TODO markers in the compute_mean in the summarize module.

---
+++ datasummarizer/datasummarizer/summarize.py
@@ -0,0 +1,15 @@
+"""Summarize data values."""
+
+from typing import List
+
+
+def compute_mean(numbers: List[float]) -> float:
+    """Compute the mean of a list of numbers."""
+    # TODO: sum the list of the numbers
+    # TODO: determine the length of the list of numbers
+    # TODO: as long as the computation will not be an
+    # undefined division by zero, compute the mean
+    # Reference: https://stackoverflow.com/questions/58400652/average-returning-a-value-even-when-list-is-empty
+    # TODO: else if the list was empty, then return a mean that is "not a number"
+    # https://stackoverflow.com/questions/944700/how-can-i-check-for-nan-values
+    # TODO: return the computed mean value
